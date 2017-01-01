bc5d3a8a6390c087333ecf97bb1ebf6a45f5eae7
omearamike <mikeytoomo@hotmail.com>
2017-01-01 22:17:04 +0000

    - Added login feature - Need to combine with old system

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'farmadmin');
+define('DB_PASSWORD', 'mike');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'aa_auth');
+
+?>
---
+++ js/authCtrl.js
@@ -0,0 +1,35 @@
+app.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location, $http, Data) {
+    //initially set those objects to null to avoid undefined error
+    $scope.login = {};
+    $scope.signup = {};
+    $scope.doLogin = function (customer) {
+        Data.post('login', {
+            customer: customer
+        }).then(function (results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+    $scope.signup = {email:'',password:'',name:'',phone:'',herdnumber:''};
+    $scope.signUp = function (customer) {
+        Data.post('signUp', {
+          // console.log($scope.signup);
+          // console.log($scope.signup);
+            customer: customer
+                      // console.log(customer);
+        }).then(function (results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+    $scope.logout = function () {
+        Data.get('logout').then(function (results) {
+            Data.toast(results);
+            $location.path('login');
+        });
+    }
+});
